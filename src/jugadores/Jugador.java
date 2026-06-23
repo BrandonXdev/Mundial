b8521a9846f2384156523035ade4d9bec7e1f682
joanrecioj <joamreciojimenez@gmail.com>
2026-06-23 15:30:15 -0600

    Se crea el paquete jugadores y la clase jugador y se programa

--- src/jugadores/Jugador.java
+++ src/jugadores/Jugador.java
@@ -4,10 +4,41 @@
  */
 package jugadores;
 
+import java.time.LocalDate;
+import personas.Persona;
+
 /**
  *
  * @author joanreciojimenez
  */
-public class Jugador {
-    
+public class Jugador extends Persona{
+ private  int numero;
+ private String posicion;
+ 
+ public int getNumero (){
+     return numero;
+ }
+ public String getPosicion(){
+     return posicion;
+ }
+ public void setNumero (int numero){
+     this.numero = numero;
+ }
+ public void setposicion(String posicion){
+     this.posicion = posicion;
+ }
+ 
+ public Jugador(String nombre,String nacionalidad, LocalDate fechaNacimiento,int numero, String posicion){
+     super (nombre,nacionalidad,fechaNacimiento);
+     this.numero = numero;
+     this.posicion = posicion;
+ }
+ 
+ @Override
+public String toString() {
+    return super.toString() + ", Numero: " + numero + ", Posicion: " + posicion;
+}
+
+ 
+ 
 }
